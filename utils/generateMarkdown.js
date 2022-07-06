912b0cb11279db183a6c3b2fe880eab6c48c4b25
justin <fezzygo@gmail.com>
2022-07-05 21:13:41 -0400

    added starter code to index.js and generateMarkdown.js

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,17 @@
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
